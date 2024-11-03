ac94be9b57ef8a207b1bc8ece3e194a23817c6de
Adarsh-Raut <adarshraut0987@gmail.com>
2024-11-03 21:09:16 +0530

    changed the resume url

--- src/data/constants.js
+++ src/data/constants.js
@@ -5,7 +5,7 @@ export const Bio = {
     'I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.',
   github: 'https://github.com/Adarsh-Raut',
   resume:
-    'https://drive.google.com/file/d/1lpWr1jJCrYqqgNWNMn58DuDXg4SKtnjq/view?usp=sharing',
+    'https://drive.google.com/file/d/1m01liXED77FuBtYgqHXwfnDcEQUsIklE/view?usp=sharing',
 };
 
 export const skills = [
